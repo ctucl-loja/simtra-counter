d7a6de804e2bb32abf6cad4707577ab361e3124f
David Diaz <david.diaz190799@gmail.com>
2026-03-25 16:56:08 -0500

    update

--- app.py
+++ app.py
@@ -105,9 +105,9 @@ def sensor_loop():
             if not (t_ing == float("inf") and t_sal == float("inf")):
                 with state_lock:
                     if t_ing <= t_sal:
-                        state["entry_counter"] += 1
-                    else:
                         state["exit_counter"] += 1
+                    else:
+                        state["entry_counter"] += 1
 
                 last_event_time = current_time
                 event_counted   = True
